4988561b6f7e16cf6b703f9cb3bbeef9cd25119f
Yanqiao4396 <chen02@allegheny.edu>
2024-01-18 14:31:30 -0500

    Clean up experimental codes

---
+++ hello_world_demo/hello_world.py
@@ -0,0 +1,6 @@
+# Pseudo Code
+
+def getNameArray():
+    nameArray = list('John','Jaclyn','Jim','Joseph')
+    for name in nameArray:
+        print(name)
---
+++ tree-sitters-in-npm/python_ex/parsee_python/python_random.py
@@ -0,0 +1,2 @@
+a = [a for a in range(5)]
+print(a)
