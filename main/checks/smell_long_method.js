122224ff4694f24f283cf79f8a241c489072830c
Yanqiao4396 <chen02@allegheny.edu>
2024-02-06 17:58:39 -0500

    factor: rename variable in smell_long_method: code_block -> code_matches

--- main/checks/smell_long_method.js
+++ main/checks/smell_long_method.js
@@ -2,11 +2,11 @@ const Parser = require('tree-sitter');
 const parser = new Parser();
 var fs = require('fs');
 
-function longMethods(code_matches, threshold){
-    for(let code_block of code_matches){
-        var node = code_block.node
+function longMethods(methods, threshold){
+    for(let mtd of methods){
+        var node = mtd.node
         if(node.endPosition.row - node.startPosition.row > threshold){
-            console.log(`Line ${node.startPosition.row}: Method is longer ${threshold} lines`)
+            console.log(`Line ${node.startPosition.row}: Method is longer than ${threshold} lines (found ${node.endPosition.row - node.startPosition.row} lines)`)
         }
 
     }
