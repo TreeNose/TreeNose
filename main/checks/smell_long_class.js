d480cfe981c31fb58afba4d42aefced652893c97
Yanqiao4396 <chen02@allegheny.edu>
2024-02-06 18:17:04 -0500

    Feat: detect long class smell

---
+++ main/checks/smell_long_class.js
@@ -0,0 +1,40 @@
+const { fetchCode } = require('./code_query')
+
+const Parser = require('tree-sitter');
+const parser = new Parser();
+
+const fs = require('fs');
+const Syntaxes = JSON.parse(fs.readFileSync('./configs/smell_categories.json', 'utf8'));
+
+function longClass(classes, nol_threshold, noc_threshold, lang){
+    for (let cls of classes){
+        clsNode = cls.node
+        var noc = checkNumberOfChildren(clsNode, lang)
+        var nol = checkNumberOfLine(clsNode)
+        
+        if (nol > nol_threshold){
+            console.log(`Line ${clsNode.startPosition.row}: Class is longer than ${nol_threshold} lines (found ${nol} lines)`)
+        }
+
+        if (noc > noc_threshold){
+            console.log(`Line ${clsNode.startPosition.row}: Class has more than ${noc_threshold} children (found ${noc} children)`)
+        }
+
+    }
+// nol: Number of line
+// noc: number of children (attributes + methods)
+}
+
+function checkNumberOfLine(clsNode){
+    return clsNode.endPosition.row - clsNode.startPosition.row
+}
+
+function checkNumberOfChildren(clsNode, lang){
+    // Doc: currently only support method_definition as children
+    const captures = fetchCode(clsNode.text, lang, "method_definition")
+    // Get the number of fields
+    const nof = new Set(captures.map((x)=>x.node.text)).size
+    return nof
+}
+
+module.exports = { longClass }
