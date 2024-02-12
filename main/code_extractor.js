b0012bae1cc82ea1ce76f61e6f56d5bb94c54ad6
Yanqiao4396 <chen02@allegheny.edu>
2024-02-12 13:53:41 -0500

    Feat: file extractor
    Extract files recursively from the current work dir based on regular expression rules

---
+++ main/code_extractor.js
@@ -0,0 +1,33 @@
+// ES6 version using asynchronous iterators, compatible with node v10.0+
+const { promises } = require("fs");
+const fs = require('fs');
+const { join } = require("path");
+const path = require('path');
+// No node modules or hidden files
+const ignoreRegex = generateIgnoreRegex()
+
+function generateIgnoreRegex() {
+    const text = fs.readFileSync('./configs/.path_ignore', 'utf8');
+    const lines = text.trim().split("\n").filter(line => line.trim() !== "");
+    const ignoreRegex ='(' + lines.join(")|(") + ')';
+    return ignoreRegex
+}
+
+async function* walk(dir, extType) {
+    for await (const d of await promises.opendir(dir)) {
+        const entry = join(dir, d.name);
+        const isIgnored = (filePath) => filePath.match(ignoreRegex);
+        if (isIgnored(entry)) continue;
+        if (d.isDirectory()) yield* walk(entry, extType);
+        else if (d.isFile() && path.extname(entry) == extType) yield entry;
+    }
+}
+
+// Then, use it with a simple async for loop
+async function main() {
+    console.log(ignoreRegex)
+    for await (const p of walk('.', '.js'))
+        console.log(p)
+}
+
+main()
