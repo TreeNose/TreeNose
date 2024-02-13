9c810f26adb9adc98f7574ea0c285e303ecdfff4
Yanqiao4396 <chen02@allegheny.edu>
2024-02-12 20:19:47 -0500

    Bug: parameters check

--- main/checks/smell_long_param.js
+++ main/checks/smell_long_param.js
@@ -4,21 +4,21 @@ const fs = require('fs');
 const Syntaxes = JSON.parse(fs.readFileSync('./configs/smell_categories.json', 'utf8'));
 
 function longParameters(methods, threshold, lang){
-    paramsSyntax = Syntaxes["node"]["parameters"][lang]
+    paramsSyntaxes = Syntaxes["node"]["parameters"][lang]
     for (let mtd of methods){
-        var paramsNode = findParametersFromMethod(mtd.node,paramsSyntax)
-        if (paramsNode.namedChildCount >  threshold){
+        var paramsNode = findParametersFromMethod(mtd.node,paramsSyntaxes)
+        if (paramsNode && paramsNode.namedChildCount >  threshold){
             console.log(`Line ${mtd.node.startPosition.row}: Method has more than ${threshold} parameters (found ${paramsNode.namedChildCount} parameters)`)
         }
     }
 }
 
-function findParametersFromMethod(methodNode, ParamsSyntax){
+function findParametersFromMethod(methodNode, ParamsSyntaxes){
     for (let subNode of methodNode.namedChildren){
-        if(subNode.type == ParamsSyntax){
+        if(ParamsSyntaxes.includes(subNode.type)){
             return subNode
         }
     }
-    new Error(`Method in line ${methodNode.startPosition.row} is not valid`)
+    return null
 }
 module.exports = {longParameters}
