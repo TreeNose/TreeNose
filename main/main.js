f72013c015da2311255cb2341d6a42cd6fa361db
Yanqiao4396 <chen02@allegheny.edu>
2024-02-27 10:46:18 -0500

    Feat: add CLI to main

--- main/main.js
+++ main/main.js
@@ -1,9 +1,10 @@
 const { fetchCode } = require('./checks/code_query')
-const fs = require('fs/promises');
+const fs_promise = require('fs/promises');
+const fs = require('fs');
 const args = require('yargs').argv
-const parsee_file = args.file
+const parsee_path = args.path
+const out_path = args.out
 const lang = args.lang
-const smell = args.smell
 
 const { longMethods } = require('./checks/smell_long_method')
 const {longParameters} = require('./checks/smell_long_param')
@@ -14,41 +15,71 @@ const {smellDB} = require('./report_generator')
 
 var SmellDataBase = new smellDB()
 
-async function main(){
+const ExtConfig = JSON.parse(fs.readFileSync('./configs/lang_suffixes.json', 'utf8'));
+
+const detectConfig = JSON.parse(fs.readFileSync('./configs/detect_config.json', 'utf8'));
+
+const longClassThreshold_nol = detectConfig.longClass.threshold_nol
+const longClassThreshold_noc = detectConfig.longClass.threshold_noc
+const longMethodThreshold = detectConfig.longMethod.threshold
+const longParameterThreshold = detectConfig.longParameter.threshold
+const longMessageChainThreshold = detectConfig.longMessageChain.threshold
+
+
+
+async function runCodeSmellDetection(){
     // for await (const target_file of walk('./example_codes/java_codes', '.java')){
-    for await (const target_file of walk('../projects/java_projects/gson', '.java')){
+    if (parsee_path == undefined || lang == undefined|| out_path == undefined){
+        console.log('Please provide a path, language and output dir to check for. e.g. node main.js --path=./example_codes/java_codes --lang=java --dest=./example_codes/csv/')
+        return
+    }
 
+    // If the path is a file
+    if (fs.statSync(parsee_path).isFile()){
+        console.log(parsee_path)
+        const src_code = await fs_promise.readFile(parsee_path, 'utf8')
+        checkCodeSmells(src_code, lang, parsee_path)
+        return
+    }
+    
+    // If the path is a directory, then walk through the directory and its subdirectories
+    for await (const target_file of walk(parsee_path, ExtConfig[lang])){
         console.log(target_file)
-        const src_code = await fs.readFile(target_file, 'utf8')
+        const src_code = await fs_promise.readFile(target_file, 'utf8')
         checkCodeSmells(src_code, 'java', target_file)
+    }
+}
 
-    }
-    console.log(SmellDataBase.getSmell('longMessageChain'))
-    }
 
 function checkCodeSmells(src_code, lang, curFile){
+
     var code_matches = fetchCode(src_code,lang,'method_definition')
-    var lmCaptured = longMethods(code_matches,100)
+    var lmCaptured = longMethods(code_matches,longMethodThreshold)
  
     var code_matches = fetchCode(src_code,lang,'method_definition')
-    var lpCaptured = longParameters(code_matches,5,lang)
+    var lpCaptured = longParameters(code_matches,longParameterThreshold,lang)
 
     var code_matches = fetchCode(src_code,lang,'class_definition')
-    var lcCaptured = longClass(code_matches,200,10,lang)
+    var lcCaptured = longClass(code_matches,longClassThreshold_nol,longClassThreshold_noc,lang)
 
     var code_matches = fetchCode(src_code,lang,'call')
-    var lmcCaptured = longMessageChains(code_matches,3, lang)
+    var lmcCaptured = longMessageChains(code_matches,longMessageChainThreshold, lang)
     
     SmellDataBase.addSmell(curFile, lmCaptured, 'longMethod')
     SmellDataBase.addSmell(curFile, lpCaptured, 'longParameter')
     SmellDataBase.addSmell(curFile, lcCaptured, 'longClass')
     SmellDataBase.addSmell(curFile, lmcCaptured, 'longMessageChain')
 
-    SmellDataBase.generateCSV('./example_codes/csv/')
+    SmellDataBase.generateCSV(out_path)
 
     return true
 }
 
 
+async function main(){
+
+    await runCodeSmellDetection()
+
+    }
 
 main()
