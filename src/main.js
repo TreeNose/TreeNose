154521529a95d4ffbebe0f7867fc377321158293
Yanqiao4396 <chen02@allegheny.edu>
2024-02-28 21:18:15 -0500

    Bug: rm hardcode for readFile

--- src/main.js
+++ src/main.js
@@ -40,45 +40,50 @@ async function runCodeSmellDetection(){
     // If the path is a file
     if (fs.statSync(parsee_path).isFile()){
         console.log(parsee_path)
-        const src_code = await fs_promise.readFile(parsee_path, 'utf8')
+        const src_code = fs.readFileSync(parsee_path, 'utf8')
         checkCodeSmells(src_code, lang, parsee_path)
         return
     }
     
     // If the path is a directory, then walk through the directory and its subdirectories
     for await (const target_file of walk(parsee_path, ExtConfig[lang])){
-        // console.log(target_file)
-        const src_code = await fs_promise.readFile(target_file, 'utf8')
-        checkCodeSmells(src_code, 'java', target_file)
+        console.log(target_file)
+        const src_code = fs.readFileSync(target_file, 'utf8')
+        checkCodeSmells(src_code, lang, target_file)
     }
 }
 
 
 function checkCodeSmells(src_code, lang, curFile){
 
-    var code_matches = fetchCode(src_code,lang,'method_definition')
-    var lmCaptured = longMethods(code_matches,longMethodThreshold)
+    // var code_matches = fetchCode(src_code,lang,'method_definition')
+    // var lmCaptured = longMethods(code_matches,longMethodThreshold)
  
-    var code_matches = fetchCode(src_code,lang,'method_definition')
-    var lpCaptured = longParameters(code_matches,longParameterThreshold,lang)
+    // var code_matches = fetchCode(src_code,lang,'method_definition')
+    // var lpCaptured = longParameters(code_matches,longParameterThreshold,lang)
 
     var code_matches = fetchCode(src_code,lang,'class_definition')
     var lcCaptured = longClass(code_matches,longClassThreshold_nol,longClassThreshold_noc,lang)
 
-    var code_matches = fetchCode(src_code,lang,'call')
-    var lmcCaptured = longMessageChains(code_matches,longMessageChainThreshold, lang)
+    if (lcCaptured.length > 0){
+        console.log(lcCaptured)
+        console.log(curFile)
+    }
+
+    // var code_matches = fetchCode(src_code,lang,'call')
+    // var lmcCaptured = longMessageChains(code_matches,longMessageChainThreshold, lang)
     
-    var ifStatements = fetchCode(src_code,lang,'if_statement')
-    var switchStatements = fetchCode(src_code,lang,'switch_statement')
-    var ccCaptured = complexConditional(ifStatements, switchStatements, complexConditionalThreshold, lang)
+    // var ifStatements = fetchCode(src_code,lang,'if_statement')
+    // var switchStatements = fetchCode(src_code,lang,'switch_statement')
+    // var ccCaptured = complexConditional(ifStatements, switchStatements, complexConditionalThreshold, lang)
 
-    SmellDataBase.addSmell(curFile, lmCaptured, 'longMethod')
-    SmellDataBase.addSmell(curFile, lpCaptured, 'longParameter')
-    SmellDataBase.addSmell(curFile, lcCaptured, 'longClass')
-    SmellDataBase.addSmell(curFile, lmcCaptured, 'longMessageChain')
-    SmellDataBase.addSmell(curFile, ccCaptured, 'complexConditional')
+    // SmellDataBase.addSmell(curFile, lmCaptured, 'longMethod')
+    // SmellDataBase.addSmell(curFile, lpCaptured, 'longParameter')
+    // SmellDataBase.addSmell(curFile, lcCaptured, 'longClass')
+    // SmellDataBase.addSmell(curFile, lmcCaptured, 'longMessageChain')
+    // SmellDataBase.addSmell(curFile, ccCaptured, 'complexConditional')
 
-    SmellDataBase.generateCSV(out_path)
+    // SmellDataBase.generateCSV(out_path)
 
     return true
 }
